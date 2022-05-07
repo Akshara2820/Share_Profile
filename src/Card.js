2c4be211927c05eb9b6c8610c8b89848a8d7800e
Akshara2820 <akshara20@navgurukul.org>
2022-05-07 14:44:18 +0530

    Api intregrate

--- src/Card.js
+++ src/Card.js
@@ -5,10 +5,45 @@ import { Component } from 'react';
 
 
 class ProfileCard extends Component {
+    //Constructor
+  constructor (props) {
+    super (props);
+
+    this.state ={
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  // ComponentDidMount is used to Exeute the code
+  componentDidMount() {
+    fetch(
+      "https://api.meetworks.in/users/get_unique_jobseeker_profile")
+      .then ((res) => res.json())
+      .then((json) =>{
+        this.setState({
+          item: json,
+          DataisLoaded: true
+        });
+      })
+  }
     render (){
+
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+          <h1> Please wait some time... </h1>
+        </div>
+
         return (
             <div className='Profile-card'>
               <Card style={{ width: '25rem'}}>
+              items.map((item) => ( 
+                <ol key = { item.id } >
+                    User_Name: { item.username }, 
+                    Full_Name: { item.name }, 
+                    User_Email: { item.email } 
+                    </ol>
+                ))
               <Card.Img variant="top" src={require('./mypic.jpg')} height={400} />
               <Card.Body>
                 <Card.Title>Suhana Parmar | Lucknow UttarPradesh</Card.Title>
